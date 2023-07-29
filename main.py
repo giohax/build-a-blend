c80688cd482d97da85860dc62b572b31202e8a3d
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-29 12:40:00 -0500

    change url

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://build-a-blend-frontend.vercel.app/"], 
+    allow_origins=["https://build-a-blend-frontend.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
