44e0bd9e9bc4ad690af0228f4a78787d4b61e6c0
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-29 14:12:55 -0500

    remove api key

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-os.environ['OPENAI_API_KEY'] = "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"
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
