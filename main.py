2d916e18dc9a7ef1eea7e53e756b2e48ed964fd0
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-29 13:23:08 -0500

    testing this part

--- main.py
+++ main.py
@@ -24,24 +24,24 @@ app.add_middleware(
 class Query(BaseModel):
     content: str
 
-# define LLM
-llm = OpenAI(temperature=0, model="gpt-3.5-turbo", max_tokens=1000)
-service_context = ServiceContext.from_defaults(llm=llm, chunk_size=51200)
 
-documents = SimpleDirectoryReader('data2').load_data()
-index = VectorStoreIndex.from_documents(documents, service_context=service_context)
-query_engine = index.as_query_engine(
+
+@app.post("/stream")
+async def stream(query: Query):
+    # define LLM
+    llm = OpenAI(temperature=0, model="gpt-3.5-turbo", max_tokens=1000)
+    service_context = ServiceContext.from_defaults(llm=llm, chunk_size=51200)
+
+    documents = SimpleDirectoryReader('data2').load_data()
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+    query_engine = index.as_query_engine(
     similarity_top_k=10
 )
-
-
-# @app.post("/stream")
-# async def stream(query: Query):
-#     query_preamble= "Given the provided product details, recommend several products from the provided data, and describe them, that satisfies the following search query or question: " 
-#     prompt = query_preamble + query.content
-#     response = query_engine.query(prompt + ". Now output this data in a numbered list without including 'Product Name' and 'Description' keywords. Then summarize everything at the end.")
+    query_preamble= "Given the provided product details, recommend several products from the provided data, and describe them, that satisfies the following search query or question: " 
+    prompt = query_preamble + query.content
+    response = query_engine.query(prompt + ". Now output this data in a numbered list without including 'Product Name' and 'Description' keywords. Then summarize everything at the end.")
     
-#     return {"response": response}
+    return {"response": response}
 
 @app.get("/")
 async def main():
